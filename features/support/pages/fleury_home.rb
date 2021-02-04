b30ac704ac61021b6c56f1e86b50bc9873a7090a
Lidia Cutri <lidiacutri@gmail.com>
2021-02-04 18:06:02 -0300

    primeiro commit do projeto

--- features/support/pages/fleury_home.rb
+++ features/support/pages/fleury_home.rb
@@ -1,10 +1,12 @@
 class FleuryHome
     include Capybara::DSL 
 
+    #comando para visitar a página
     def go
         visit '/'
     end
 
+    #comando para acessar os menus
     def select_menu_unidades
         click_link 'Unidades'
     end
